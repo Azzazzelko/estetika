31b39bb6cbb82887a72222d3cd5ad610c3b4c1bb
Vladislav <azzazzelko@gmail.com>
2017-02-06 18:00:58 +0300

    replace fonts, some modals, doctors-page

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,38 @@ $(function() {
 		dots:true
 	});
 
+	$('.gallery-pop-up').on("show.bs.modal", function(e){
+
+		$('.owl-gallery-slider').owlCarousel({
+			loop:true,
+			margin:0,
+			items:1,
+			nav:true,
+			dots:true
+		});
+	});
+
+	/***** forms ****/
+
+	var materialForm = function() {
+		$('.material-field').focus(function() {
+			$(this).closest('.form-group-material').addClass('focused has-value');
+		}).focusout(function() {
+			$(this).closest('.form-group-material').removeClass('focused');
+		}).blur(function() {
+			if (!this.value) {
+				$(this).closest('.form-group-material').removeClass('has-value');
+			}
+			$(this).closest('.form-group-material').removeClass('focused');
+		});
+	};
+
+	materialForm();
+
+	// $('textarea').on("keyup", function(e){
+	// 	this.style.height = "1px";
+	// 	this.style.height = (5+this.scrollHeight)+"px";
+	// 	this.scrollHeight > 60 ? this.style.overflow = "auto" : this.style.overflow = "hidden";
+	// });
+
 }());
