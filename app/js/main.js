e726c46edae8524269cbbda0d2270ca22c1c61ca
Vladislav <azzazzelko@gmail.com>
2017-02-14 14:45:54 +0300

    article adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -79,4 +79,112 @@ $(function() {
 
 	materialForm();
 
+	/******************
+	**  validation   **
+	******************/
+
+	var validation = (function(){
+
+		var inputs = $("form").find('input, textarea'); //инпуты в формах
+
+		var init = function(){
+			setUpListeners();
+		};
+
+		var setUpListeners = function(){
+			inputs.on('keydown', function(e){ 	        //удаляем ошибки при нажатии клавишь на поле ввода
+				var $this = $(this);
+				$this.removeClass('input_error');
+			});
+
+			$("form").on("submit", function(e){
+				e.preventDefault();
+
+				( isValid($(this)) ) ? console.log("well done!") : console.log("valid fail!");
+			});
+		};
+
+		function isValid(thisForm){
+
+			var isValid = true;
+
+			thisForm.find('input, textarea').each(function(i){
+
+				var $this = $(this),
+				thisVal = $this.val();
+
+				if ($this.prop('required')) {
+
+					if ($this.is('input[type="email"]')) {
+
+						if (validateEmail(thisVal)) {
+
+							$this.removeClass('input_error');
+							return isValid = true;
+
+						} else {
+
+							$this.addClass('input_error');
+							showErrors($this);
+							return isValid = false;
+						}
+
+					}
+
+					if (!thisVal.trim()) {
+
+						$this.addClass('input_error');
+						showErrors($this);
+						return isValid = false;
+
+					} else {
+
+						$this.removeClass('input_error');
+						return isValid = true;
+
+					}	
+
+				}
+
+			});
+
+			return isValid;
+		};
+
+		function validateEmail(email){
+			var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+			return re.test(email);
+		};
+
+		function showErrors(elem){
+			elem.qtip({ 
+				content: {
+					text: 'Нужно заполнить все поля' 
+				},
+				position: {
+					my: 'left center', 
+					at: 'right center',
+					adjust: {
+						x: 40
+					}
+				}, 
+				show: {
+					ready: true,
+					event: false
+				},
+				hide: {
+					event: 'focus'
+				}
+			})
+		};
+
+		return {
+			init:init
+		}
+	}());
+
+	$(document).ready(function(){
+		validation.init();
+	});
+
 }());
